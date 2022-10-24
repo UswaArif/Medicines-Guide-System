d8395926b437b6db5ff3d6e34fe6083d98c5439b
mahnoorhassan123 <mahnoorh344@gmail.com>
2022-10-24 14:13:29 +0500

    Update insertionSort.py

--- insertionSort.py
+++ insertionSort.py
@@ -1,16 +1,21 @@
-def InsertionSort(array, start, end):
-    Arr = []
-    for i in range(start, end):
+def InsertionSort(array):                    # sort in ascending
+    for i in range(1, len(array)):
         key = array[i]
         j = i - 1
         while key < array[j] and j >= 0:
             array[j + 1] = array[j]
             j = j - 1
         array[j + 1] = key    
-    for i in range(start, end):
-        Arr.append(array[i])
-    return Arr
+    return array
 
 
-
-
+def InsertionSortDescending(array):         # sort in descending
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i - 1
+        while key < array[j] and j >= 0:
+            array[j + 1] = array[j]
+            j = j - 1
+        array[j + 1] = key    
+    array.reverse()
+    return array
