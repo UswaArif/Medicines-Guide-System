296dc181fd09ad2986bbb8dbece7959567ea62c0
mahnoorhassan123 <mahnoorh344@gmail.com>
2022-10-24 14:29:55 +0500

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,11 +1,19 @@
-def BubbleSort(array, start, end):
-    for i in range(start, end):
-        for j in range(i + 1, end):
+def BubbleSort(array):
+    for i in range(0, len(array)):
+        for j in range(i + 1, len(array)):
             if array[i] > array[j]:
                 temp = array[i]
                 array[i] = array[j]
                 array[j] = temp
-    Arr = []
-    for i in range(start, end):
-        Arr.append(array[i])
-    return Arr
+    return array
+
+
+def BubbleSortDescending(array):
+    for i in range(0, len(array)):
+        for j in range(i + 1, len(array)):
+            if array[i] > array[j]:
+                temp = array[i]
+                array[i] = array[j]
+                array[j] = temp
+    array.reverse()
+    return array
