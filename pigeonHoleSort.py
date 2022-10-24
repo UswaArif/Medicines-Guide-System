6fc0bed7a120f8e0dff6edc910abd923c67695b2
mahnoorhassan123 <mahnoorh344@gmail.com>
2022-10-24 14:18:26 +0500

    Update pigeonHoleSort.py

--- pigeonHoleSort.py
+++ pigeonHoleSort.py
@@ -1,28 +1,33 @@
-def pigeonhole_sort(a):
-    my_min = min(a)
-    my_max = max(a)
-    size = my_max - my_min + 1
-  
-    holes = [0] * size   #   holes=[0 for x in range(numberOfHoles)]
-  
-    for x in a:
-       # assert type(x) is int, "integers only please"
-    
-        holes[x - my_min] += 1
+def PigeonHoleSort(array):              
+    minNum = min(array)
+    maxNum = max(array)
+    size = maxNum - minNum + 1
+    holes = [0 for x in range(size)]
+    for x in array:
+        holes[x - minNum] += 1
   
     i = 0
     for count in range(size):               
         while holes[count] > 0:
             holes[count] -= 1
-            a[i] = count + my_min
+            array[i] = count + minNum
             i += 1
-    return a
-              
-#  sortedArray=[]
-#    for count in range(numberOfHoles):
- #       while holes[count]>0:
-  #          holes[count] -=1
-   #         sortedArray.append(count+smallestlestInteger)
-   
-A = [22,1,3,9,6,0,3,5,8]
-print(pigeonhole_sort(A))
+    return array
+
+
+def PigeonHoleSortDescending(array):              
+    minNum = min(array)
+    maxNum = max(array)
+    size = maxNum - minNum + 1
+    holes = [0 for x in range(size)]
+    for x in array:
+        holes[x - minNum] += 1
+  
+    i = 0
+    for count in range(size):               
+        while holes[count] > 0:
+            holes[count] -= 1
+            array[i] = count + minNum
+            i += 1
+    array.reverse()
+    return array
