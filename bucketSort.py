a885ab7b75706713a2f10ce35d50e3fa16670b2a
mahnoorhassan123 <mahnoorh344@gmail.com>
2022-10-24 14:34:40 +0500

    Update bucketSort.py

--- bucketSort.py
+++ bucketSort.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 20 14:11:16 2022
-
-@author: HP
-"""
 def insertion_sort(Arr):
     for i in range(1,len(Arr),1):
         key = Arr[i]
@@ -42,7 +36,32 @@ def bucketSort(Array):
         result = result + buckets[i]
              
     return result
+
+
+def bucketSortDescending(Array):
+    largest = max(Array)
+    length = len(Array)
+    size = largest/length
+ 
+    # Create Empty Buckets array
+    buckets = [[] for i in range(length)]
+ 
+    # Bucket Sorting   
+    for i in range(length):
+        index = int(Array[i]/size)
+        if index != length:
+            buckets[index].append(Array[i])
+        else:
+            buckets[length - 1].append(Array[i])
+  
+    #Sorting Array with insertion sort
+    for i in range(len(Array)):
+        insertion_sort(buckets[i])
  
  
-Arr = [5, 0, 2, 7, 8, 55, -2, 111, 4, 5, 1, 2]
-print(bucketSort(Arr))
+    # Concatenating the bucket Array
+    result = []
+    for i in range(length):
+        result = result + buckets[i]
+    result.reverse()         
+    return result
